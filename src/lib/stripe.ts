146958beddc3ed96c08bbef89bad5df8b5c3da56
sébastien hamel <sebastienhamel@Mac.lan>
2026-01-06 22:32:46 -0500

    Use dummy key for stripe build

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from 'stripe';
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? '', {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || 'sk_test_dummy', {
     apiVersion: '2025-12-15.clover', // Use the latest API version or the one matching your account
     typescript: true,
 });
