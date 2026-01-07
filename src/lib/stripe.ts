3dcd30bb074c9ee37c4983be858dce0c689027d8
sébastien hamel <sebastienhamel@Mac.lan>
2026-01-06 22:30:38 -0500

    Relax stripe key check for build

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,10 +1,6 @@
 import Stripe from 'stripe';
 
-if (!process.env.STRIPE_SECRET_KEY) {
-    throw new Error('STRIPE_SECRET_KEY is missing. Please set it in your .env file.');
-}
-
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? '', {
     apiVersion: '2025-12-15.clover', // Use the latest API version or the one matching your account
     typescript: true,
 });
