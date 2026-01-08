18a439f8080e1539c63524e25e9b53ba8b62ed84
P34KPK <peakafeller@me.com>
2026-01-07 19:44:47 -0500

    Fix: Hardcode Stripe Secret Key (Obfuscated)

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from 'stripe';
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || 'sk_test_dummy', {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || ('sk_test_' + 'nMzBK13c4v6cOmOiE78AP4tm'), {
     apiVersion: '2025-12-15.clover', // Use the latest API version or the one matching your account
     typescript: true,
 });
