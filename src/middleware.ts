e51ec291d242fddf34d640ed0e1e011d3870aa1b
P34KPK <deeplink@p34k.compeakafeller@me.com>
2026-01-09 16:21:42 -0500

    Completely exclude webhooks from Middleware matcher to prevent 401 Auth errors

--- src/middleware.ts
+++ src/middleware.ts
@@ -63,9 +63,9 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
     matcher: [
-        // Skip Next.js internals, static files, AND /amzn routes
-        '/((?!_next|amzn|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        // Always run for API routes
-        '/(api|trpc)(.*)',
+        // Skip Next.js internals, static files, and api/webhooks
+        '/((?!_next|amzn|api/webhooks|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+        // Always run for API routes EXCEPT webhooks
+        '/(api|trpc)((?!/webhooks).*)',
     ],
 };
